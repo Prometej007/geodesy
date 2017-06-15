963eec8b12e6f88f7a50f825d6c38c2ceb7bbe26
prometej <nazar-danilyak@mail.ru>
2017-06-15 19:20:59 +0300

    add packeg model persistence service

---
+++ src/main/java/com/geodesy/web/geodesy/model/delete.java
@@ -0,0 +1,8 @@
+package com.geodesy.web.geodesy.model;
+
+/**
+ * Created by danul on 15.06.2017.
+ */
+public class delete
+{
+}
---
+++ src/main/java/com/geodesy/web/geodesy/persistence/delete.java
@@ -0,0 +1,7 @@
+package com.geodesy.web.geodesy.persistence;
+
+/**
+ * Created by danul on 15.06.2017.
+ */
+public class delete {
+}
